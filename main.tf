0f9d6ce887e5d326e837c14c9b7bb184dc1d98f9
greyhoundforty <ryantiffany@fastmail.com>
2024-01-23 12:37:31 -0600

    Adding prefix

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source                       = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.1.1"
-  resource_group_name          = var.existing_resource_group == null ? "${local.prefix}-resource-group" : null
+  resource_group_name          = var.existing_resource_group == null ? "${rst}-resource-group" : null
   existing_resource_group_name = var.existing_resource_group
 }
 
